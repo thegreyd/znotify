3ac40c4dae810e702350f1abcf65ee0778f788d6
Siddharth Sharma <sid.sharma0@gmail.com>
2015-05-08 01:29:25 +0530

    Adjusted to age attribute

--- parser.py
+++ parser.py
@@ -1,18 +1,19 @@
 from bs4 import BeautifulSoup
+from datetime import datetime
 from torrent import Torrent
 
-months={"Jan":"01",
-        "Feb":"02",
-        "Mar":"03",
-        "Apr":"04",
-        "May":"05",
-        "Jun":"06",
-        "Jul":"07",
-        "Aug":"08",
-        "Sep":"09",
-        "Oct":"10",
-        "Nov":"11",
-        "Dec":"12"
+months={"Jan":1,
+        "Feb":2,
+        "Mar":3,
+        "Apr":4,
+        "May":5,
+        "Jun":6,
+        "Jul":7,
+        "Aug":8,
+        "Sep":9,
+        "Oct":10,
+        "Nov":11,
+        "Dec":12
     }
 
 class MyParser():
@@ -28,20 +29,31 @@ class MyParser():
             categ=str(it.category.string)
             
             pub_Date = str(it.pubDate.string).split()
-            year = pub_Date[3]
-            month = months[pub_Date[2]]
-            day = pub_Date[1]
-            day = "0" + day if int(day) < 10 else day
-            time = pub_Date[4]
-            re_Date = "{}/{}/{} {}".format(year,month,day,time)
+            year = int(pub_Date[3])
+            nmon = months[pub_Date[2]]
+            nday = int(pub_Date[1])
             
+            month = "0" + str(nmon) if nmon < 10 else str(nmon)
+            day = "0" + str(nday) if nday < 10 else str(nday)
+
+            time = pub_Date[4]
+            time_S = time.split(":")
+            hours = int(time_S[0])
+            minutes = int(time_S[1])
+            seconds = int(time_S[2])
+
+            re_Date = "{}/{}/{} {}".format(year,month,day,time)
+
+            date_Time_Obj = datetime(year, nmon, nday, hours, minutes, seconds )
+            time_Duration = str(date_Time_Obj.utcnow() - date_Time_Obj)
+
             desc = str(it.description.string).split()
             size = "{} {}".format(desc[1],desc[2])
             seeds = int(desc[4].replace(",",""))
             peers = int(desc[6].replace(",",""))
             thash = desc[8]
             
-            a_Torrent = Torrent(thash,title,re_Date,categ,size,seeds,peers)
+            a_Torrent = Torrent(thash=thash,title=title,pub_Date=re_Date,categ=categ,size=size,seeds=seeds,peers=peers,age=time_Duration)
             torrents_List.append(a_Torrent)
 
         return torrents_List        
